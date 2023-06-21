a42ed73d0e424cbfdd632be84b00cb2ad0a9c43c
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-21 15:14:59 -0700

    please work ong

--- index.js
+++ index.js
@@ -2,32 +2,17 @@ const express = require("express");
 const app = express();
 
 app.post("/interactions", (req, res) => {
-    const nacl = require('tweetnacl');
 
 // Your public key can be found on your application in the Developer Portal
 const PUBLIC_KEY = '1fe8e3e4ed23a6426ecdc9bfb16d24714b54460e5fdaf338d507ed2d5b6d181d';
 
 const signature = req.get('X-Signature-Ed25519');
-const timestamp = req.get('X-Signature-Timestamp');
-const body = req.rawBody; // rawBody is expected to be a string, not raw bytes
-
-const isVerified = nacl.sign.detached.verify(
-  Buffer.from(timestamp + body),
-  Buffer.from(signature, 'hex'),
-  Buffer.from(PUBLIC_KEY, 'hex')
-);
-
-if (!isVerified) {
-  return res.status(401).end('invalid request signature');
-}
-
-if(req.body.type == 1) {
-    res.status(200).send(
-        JSON.stringify({
-          type: 1
-        })
-      )
-}
+ const timestamp = req.get('X-Signature-Timestamp');
+ const isValidRequest = verifyKey(req.rawBody, signature, timestamp, PUBLIC_KEY);
+ if (!isValidRequest) {
+   return res.status(401).end('Bad request signature');
+ }
+ let message = req.body
 })
 
 app.listen(5000, () => {
