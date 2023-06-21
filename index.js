321c0dfe5477437e7bad43b09703b45a991b4090
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-21 14:57:54 -0700

    serverless bot

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 
+app.get("/interactions", (req, res) => {
+    const nacl = require('tweetnacl');
 
-app.get("/yo", (req, res) => {
-  res.send("Express on Vercel");
-});
+// Your public key can be found on your application in the Developer Portal
+const PUBLIC_KEY = '1fe8e3e4ed23a6426ecdc9bfb16d24714b54460e5fdaf338d507ed2d5b6d181d';
+
+const signature = req.get('X-Signature-Ed25519');
+const timestamp = req.get('X-Signature-Timestamp');
+const body = req.rawBody; // rawBody is expected to be a string, not raw bytes
+
+const isVerified = nacl.sign.detached.verify(
+  Buffer.from(timestamp + body),
+  Buffer.from(signature, 'hex'),
+  Buffer.from(PUBLIC_KEY, 'hex')
+);
+
+if (!isVerified) {
+  return res.status(401).end('invalid request signature');
+}
+})
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
