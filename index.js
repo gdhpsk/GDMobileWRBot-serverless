065987f8ea3ae53cbb587261e4ffe48f49af8d55
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-21 15:33:29 -0700

    please work

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-const { verifyKey } = require("discord-interactions");
 const express = require("express");
 const app = express();
 
-app.post("/interactions", (req, res) => {
+const tweetnacl = require('tweetnacl');
+const naclUtil = require('tweetnacl-util');
+
+// Verify the interaction signature
+const PUBLIC_KEY = '1fe8e3e4ed23a6426ecdc9bfb16d24714b54460e5fdaf338d507ed2d5b6d181d';
+function verifySignature(signature, timestamp, rawBody, publicKey) {
+  const message = naclUtil.decodeUTF8(timestamp + rawBody);
+  const publicKeyUint8 = naclUtil.decodeBase64(publicKey);
+  const signatureUint8 = naclUtil.decodeBase64(signature);
+
+  try {
+    return tweetnacl.sign.detached.verify(message, signatureUint8, publicKeyUint8);
+  } catch (error) {
+    return false;
+  }
+}
+
+app.post("/interactions", async (req, res) => {
 
 // Your public key can be found on your application in the Developer Portal
-const PUBLIC_KEY = '1fe8e3e4ed23a6426ecdc9bfb16d24714b54460e5fdaf338d507ed2d5b6d181d';
 
+const interaction = req.body;
+
+// Verify the interaction's signature
 const signature = req.get('X-Signature-Ed25519');
- const timestamp = req.get('X-Signature-Timestamp');
- const isValidRequest = verifyKey(req.rawBody, signature, timestamp, PUBLIC_KEY);
- if (!isValidRequest) {
-   return res.status(401).end('Bad request signature');
- }
- let message = req.body
+const timestamp = req.get('X-Signature-Timestamp');
+const isValidSignature = verifySignature(signature, timestamp, req.rawBody, PUBLIC_KEY);
+
+if (!isValidSignature) {
+  return res.status(401).end();
+}
+
+switch (interaction.type) {
+    case 1: // Ping
+      res.json({ type: 1 });
+      break;
+    case 2: 
+    res.status(200).end();
+      break;
+    default:
+      res.status(400).end();
+      break;
+  }
+
+
 })
 
 app.listen(5000, () => {
