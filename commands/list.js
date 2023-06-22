f4e4032520cf062af35ce2a912b590770a868a7f
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-06-22 15:58:43 -0700

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -90,7 +90,7 @@ module.exports = {
                     }
                 ]
             }
-            console.log(components)
+            console.log(components.components)
         }
         }
         if(level) {
